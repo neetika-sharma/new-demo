cf7f4e65763d24552e20f89557301b528d72d518
sharnitika <sharmaneetika988@gmail.com>
2019-10-20 12:08:49 +0530

    My demo comment

---
+++ playing with datatypes.R
@@ -0,0 +1,41 @@
+3+4
+
+a=4
+a
+# rm() required bcz R does not override variable names as python
+rm(a)
+typeof(a)
+class(a)
+# 1*10^41
+a=200000000000000000000000000000000000000000 
+a
+typeof(a)
+class(a)
+
+
+b <- 'nitika hi'
+b
+typeof(b)
+class(b)
+length(b)
+attributes(b)
+
+# if you write some decimal value or fraction value then compiler will 
+#automatically will be going to convert it from integer to numeric (double)
+assign('x',2L)
+x
+typeof(x)
+#integer
+class(x)
+#integer
+
+h=-23
+h
+typeof(h)
+#double
+class(h)
+#numeric
+
+
+
+
